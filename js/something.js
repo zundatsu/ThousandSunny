73e5f756b1a6ce4aac5750ad2078e128cf7f5293
zundatsu <94760437+zundatsu@users.noreply.github.com>
2021-11-30 07:36:47 +0300

    adaptive res & js test

---
+++ js/something.js
@@ -0,0 +1,14 @@
+$('.buttonnext').click(function(){
+    //$(this).parent().find('.button').addClass('off');
+    $(this).addClass('nextpressed');
+    $('.buttonprev').removeClass('prevpressed');
+    $('.g5_images-next').addClass('secondpicture');
+    $('.g5_images-next').removeClass('firstpicture');
+    });
+$('.buttonprev').click(function(){
+    //$(this).parent().find('.button').addClass('off');
+    $(this).addClass('prevpressed');
+    $('.buttonnext').removeClass('nextpressed');
+    $('.g5_images-next').addClass('firstpicture');
+    $('.g5_images-next').removeClass('secondpicture');
+    });
